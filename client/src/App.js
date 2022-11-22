32e71b0251a448736cfbf2088c0ba26845cd7ba8
celinaxiong <celinaxiong0321@gmail.com>
2022-11-22 12:28:03 -0800

    p

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,11 @@ import InsertFood from './components/InsertFood';
 import SelectMember from './components/SelectMember';
 
 function App() {
-<<<<<<< HEAD
   return (
-    <Fragment>
-      <div className = "container">
-      <InsertFood />
-      </div>
-    </Fragment>
-  );
-=======
-  return 
   <Fragment>
     <InsertFood></InsertFood>
-    </Fragment>;
->>>>>>> bf840818925905f64d0f04518d55782da1ba4d5b
+    </Fragment>)
+  
 }
 
 export default App;
--- client/src/components/InsertFood.js
+++ client/src/components/InsertFood.js
@@ -1,11 +1,29 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
 const InsertFood = () => {
+
+    const [description, setDescription] = useState("Add Food Here...");
+
+    const onSubmitForm = async e () => {
+        e.preventDefault();
+        try {
+            const body = {description};
+            const response = fetch("http://localhost:3000/food", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     return (
         <Fragment>
             <h1 className = "text-center mt-5">blahh Food</h1>
             <form className = "d-flex mt-5">
-                <input type = "text" className = "form-control" />
+                <input type = "text" className = "form-control" value = {description}
+                onChange = {e => setDescription(e.target.value)}/>
                 <button className = "btn btn-success">Insert</button>
             </form>
     </Fragment>
