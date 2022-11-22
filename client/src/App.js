d97479ad74a3005d90626087575ba9c5ef8c12c2
celinaxiong <celinaxiong0321@gmail.com>
2022-11-22 12:12:08 -0800

    added buttons for add food

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import { Fragment } from 'react';
+import React, {Fragment} from "react";
 import './App.css';
 
 // components 
 import InsertFood from './components/InsertFood';
 
 function App() {
-  return <Fragment><InsertFood></InsertFood></Fragment>;
+  return (
+    <Fragment>
+      <div className = "container">
+      <InsertFood />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
--- client/src/components/InsertFood.js
+++ client/src/components/InsertFood.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 const InsertFood = () => {
-    return <h1>Insert Food</h1>;
+    return (
+        <Fragment>
+            <h1 className = "text-center mt-5">blahh Food</h1>
+            <form className = "d-flex mt-5">
+                <input type = "text" className = "form-control" />
+                <button className = "btn btn-success">Insert</button>
+            </form>
+    </Fragment>
+    );
+    
 };
 
 export default InsertFood;
