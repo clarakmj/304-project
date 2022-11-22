60f3f29f7a075e971f23c46b4657ce2854954745
celinaxiong <celinaxiong0321@gmail.com>
2022-11-22 11:36:02 -0800

    p

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import { Fragment } from 'react';
 import './App.css';
 
+// components 
+import InsertFood from './components/InsertFood';
+
 function App() {
-  return <Fragment></Fragment>;
->>>>>>> 1c38f7ca32718cf23a4aaac6daaedd5a453143ca
+  return <Fragment><InsertFood></InsertFood></Fragment>;
 }
 
 export default App;
