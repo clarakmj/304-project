a347df8d89fb3627ef08af73acb7de224280bf3c
celinaxiong <celinaxiong0321@gmail.com>
2022-11-21 18:34:49 -0800

    built routes

--- server/index.js
+++ server/index.js
@@ -9,14 +9,69 @@ app.use(express.json());
 // routes
 
 // create member
+app.post("/member", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO gym (description) VALUES($1) RETURNING *", [description]);
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 // create employee
+app.post("/employee", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO manager (description) VALUES($1) RETURNING *", [description]);
+        res.json(newTodo);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 // create equipment
+app.post("/equipment", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO equipment (description) VALUES($1) RETURNING *", [description]);
+        res.json(newTodo);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+// create cafe
+app.post("/cafe", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO cafe (description) VALUES($1) RETURNING *", [description]);
+        res.json(newTodo);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
 
 // get all members
+app.get("/member", async (req, res) => {
+    try {
+        const allMembers = await pool.query("SELECT * FROM member");
+        res.json(allMembers.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 // get all employees
+app.get("/member", async (req, res) => {
+    try {
+        const allEmployees = await pool.query("SELECT * FROM employee");
+        res.json(allEmployees.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 // get cafe
 
