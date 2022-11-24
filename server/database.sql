374b3fb928afae502823ca55f29cffc8e4e0237c
clarakmj <clarakmj@students.cs.ubc.ca>
2022-11-24 10:50:39 -0800

    added more data

--- server/database.sql
+++ server/database.sql
@@ -117,7 +117,8 @@ values
 (222, 200, 'Victoria', 23456),
 (333, 100, 'Vancouver', 34567),
 (444, 120, 'Surrey', 45678),
-(555, 50, 'Vancouver', 56789);
+(555, 50, 'Vancouver', 56789),
+(666, 20, 'Kamloops', 67890);
 
 insert into member
 (memid, phonenum, streetaddr, memname, membershipnum, branchnum)
@@ -126,7 +127,8 @@ values
 (22, 8881112222, '223 Test Avenue', 'John Dee', 1001, 444),
 (33, 8881231212, '990 Fake Street', 'Phil Frank', 1002, 333),
 (44, 8881998823, '102 Random Boulevard', 'Sara Jones', 1003, 222),
-(55, 8882222277, '98 Hello World', 'Meredith Grey', 1004, 111);
+(55, 8882222277, '98 Hello World', 'Meredith Grey', 1004, 111),
+(66, 2842349877, '99 Street Name', 'Blicker Jones', 1005, 555);
 
 insert into manager
 (mid, mname, gymnum)
@@ -135,7 +137,8 @@ values
 (23456, 'Sara', 222),
 (34567, 'Ash', 333),
 (45678, 'Bill', 444),
-(56789, 'Avery', 555);
+(56789, 'Avery', 555),
+(67890, 'Cait', 666);
 
 insert into trainer
 (tid, tname)
@@ -144,7 +147,10 @@ values
 (393, 'Grace'),
 (321, 'Warren'),
 (251, 'Gary'),
-(920, 'Trish');
+(920, 'Trish'),
+(295, 'Bella'),
+(314, 'Yan'),
+(934, 'Yan');
 
 insert into membership
 (memnum, expirydate, memid, amenityaccess, hasmassage, hasfitnessclass)
@@ -153,7 +159,8 @@ values
 (1001, '12/12/2023', 22, false, false, true),
 (1002, '02/01/2023', 33, false, false, false),
 (1003, '01/01/2024', 44, true, false, true),
-(1004, '05/12/2023', 55, false, true, true);
+(1004, '05/12/2023', 55, false, true, true),
+(1005, '01/01/2025', 66, true, true, true);
 
 insert into equipment 
 (serialnum, ename, etype, estatus)
@@ -162,7 +169,9 @@ values
 (13131, 'Kettlebell', 'Weight', 'Enroute'), 
 (23232, 'Treadmill', 'Cardio', 'Available'), 
 (45677, 'Stairmaster', 'Cardio', 'Available'), 
-(23432, 'Rowing Machine', 'Cardio', 'Repair');
+(23432, 'Rowing Machine', 'Cardio', 'Repair'),
+(92882, 'Bench', 'Strength', 'Enroute'),
+(19201, 'Barbell', 'Strength', 'Available');
 
 insert into cafe
 (storenum, branchnum)
@@ -172,6 +181,7 @@ values
 (329857, 333),
 (398470, 444),
 (273463, 555); 
+(192931, 666);
 
 insert into food
 (fid, price, storenum, branchnum)
@@ -180,7 +190,8 @@ values
 (3997, 10.00, 398470, 222),
 (3883, 8.98, 329857, 333),
 (2412, 5.59, 398470, 444),
-(1269, 14.49, 273463, 555); 
+(1269, 14.49, 273463, 555),
+(1092, 25.99, 192931, 666);
 
 insert into worksat
 (branchnum, tid, employmenttype)
@@ -189,7 +200,10 @@ values
 (222, 251, 'Part-Time'),
 (333, 321, 'Casual'),
 (444, 393, 'Full-Time'),
-(555, 920, 'Casual');
+(555, 920, 'Casual'),
+(666, 314, 'Part-Time'),
+(333, 295, 'Full-Time'),
+(111, 934, 'Full-Time');
 
 insert into trains
 (tid, memid)
@@ -199,6 +213,7 @@ values
 (321, 33),
 (393, 44),
 (920, 55);
+(314, 66);
 
 insert into uses
 (routine, serialnum, memid)
@@ -208,6 +223,7 @@ values
 ('Burst training', 13131, 33),
 ('Rowing', 23432, 44),
 ('Booty building', 45677, 55);
+('Strength training', 19201, 66);
 
 insert into buys
 (memid, fid)
@@ -217,5 +233,6 @@ values
 (33, 3434),
 (44, 3883),
 (55, 3997);
+(66, 1092);
 
 commit;
